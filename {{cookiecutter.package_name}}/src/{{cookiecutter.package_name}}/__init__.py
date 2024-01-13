c324082f53155dd06ba037c62c860a4a51b0713c
Hariharan Srinath <srinathh@gmail.com>
2024-01-13 23:06:08 +0800

    package sources

---
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -0,0 +1,7 @@
+"""{{ cookiecutter.package_name }} - {{ cookiecutter.package_description }}"""
+
+__version__ = '{{ cookiecutter.package_version }}'
+__author__ = '{{ cookiecutter.author_name }} <{{ cookiecutter.author_email }}>'
+
+def hello_world():
+    return "Hello World!"
